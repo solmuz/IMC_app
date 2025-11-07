a658e359daf5ab1904d3494e12c71162d2a9c00c
daniel <jose.daniel.munoz@hotmail.com>
2025-11-07 09:36:27 -0600

    js cambio

--- database.js
+++ database.js
@@ -4,9 +4,8 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: 'Admin',
-    database: 'ivg_db'
+    database: 'imc_app'
 }).promise();
 
-const result = await pool.query("SELECT * FROM users")
-
-console.log(result);
+const [rows] = await pool.query("SELECT * FROM users");
+console.log(rows);
