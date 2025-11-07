0a2353303fad529ef008a556beadf0a1baff6df9
daniel <jose.daniel.munoz@hotmail.com>
2025-11-07 09:48:43 -0600

    agg .env

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Admin',
-    database: 'imc_app'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise();
 
 const [rows] = await pool.query("SELECT * FROM users");
